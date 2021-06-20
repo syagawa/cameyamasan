d2552ae5242d30a61ecac92f6fb79675b4cd8ce6
syagawa <syagawapp@gmail.com>
2021-06-20 21:56:10 +0900

    Cancel some handlers

--- app_httpd.cpp
+++ app_httpd.cpp
@@ -665,12 +665,12 @@ static esp_err_t index_handler(httpd_req_t *req){
 void startCameraServer(){
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
-    httpd_uri_t index_uri = {
-        .uri       = "/",
-        .method    = HTTP_GET,
-        .handler   = index_handler,
-        .user_ctx  = NULL
-    };
+    // httpd_uri_t index_uri = {
+    //     .uri       = "/",
+    //     .method    = HTTP_GET,
+    //     .handler   = index_handler,
+    //     .user_ctx  = NULL
+    // };
 
     httpd_uri_t status_uri = {
         .uri       = "/status",
@@ -700,12 +700,12 @@ void startCameraServer(){
         .user_ctx  = NULL
     };
 
-   httpd_uri_t stream_uri = {
-        .uri       = "/stream",
-        .method    = HTTP_GET,
-        .handler   = stream_handler,
-        .user_ctx  = NULL
-    };
+//    httpd_uri_t stream_uri = {
+//         .uri       = "/stream",
+//         .method    = HTTP_GET,
+//         .handler   = stream_handler,
+//         .user_ctx  = NULL
+//     };
 
 
     ra_filter_init(&ra_filter, 20);
@@ -728,7 +728,7 @@ void startCameraServer(){
     
     Serial.printf("Starting web server on port: '%d'\n", config.server_port);
     if (httpd_start(&camera_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(camera_httpd, &index_uri);
+        // httpd_register_uri_handler(camera_httpd, &index_uri);
         httpd_register_uri_handler(camera_httpd, &cmd_uri);
         httpd_register_uri_handler(camera_httpd, &status_uri);
         httpd_register_uri_handler(camera_httpd, &capture_uri);
@@ -738,7 +738,7 @@ void startCameraServer(){
     config.server_port += 1;
     config.ctrl_port += 1;
     Serial.printf("Starting stream server on port: '%d'\n", config.server_port);
-    if (httpd_start(&stream_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(stream_httpd, &stream_uri);
-    }
+    // if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    //     httpd_register_uri_handler(stream_httpd, &stream_uri);
+    // }
 }
