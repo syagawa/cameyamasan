777ec0e2d27fca374c4008be94b115ee4f3a32ee
syagawa <syagawapp@gmail.com>
2021-06-28 22:33:21 +0900

    Fix shot.py

--- py/shot.py
+++ py/shot.py
@@ -35,42 +35,55 @@ def shot(ip):
     "val": "4"
   }
 
+  params = {
+    "fs": "1",
+    "q": "4"
+  }
+
 
   set_url = f"http://{ip}/control"
   capture_url = f"http://{ip}/cap"
   status_url = f"http://{ip}/status"
 
-  req1 = urllib.request.Request('{}?{}'.format(set_url, urllib.parse.urlencode(framesize_params)))
-  print(req1.full_url)
-  req2 = urllib.request.Request('{}?{}'.format(set_url, urllib.parse.urlencode(quality_params)))
-  print(req2.full_url)
+  # req1 = urllib.request.Request('{}?{}'.format(set_url, urllib.parse.urlencode(framesize_params)))
+  # print(req1.full_url)
+  # req2 = urllib.request.Request('{}?{}'.format(set_url, urllib.parse.urlencode(quality_params)))
+  # print(req2.full_url)
 
-  req3 = urllib.request.Request(status_url)
-  print(req3.full_url)
+  # req3 = urllib.request.Request(status_url)
+  # print(req3.full_url)
 
-  req4 = urllib.request.Request(capture_url)
-  print(req4.full_url)
+  # req4 = urllib.request.Request(capture_url)
+  # print(req4.full_url)
 
-  with urllib.request.urlopen(req1) as res1:
-    print(res1)
+  # with urllib.request.urlopen(req1) as res1:
+  #   print(res1)
   
-  sleep(1)
+  # sleep(1)
 
-  with urllib.request.urlopen(req2) as res2:
-    print(res2)
+  # with urllib.request.urlopen(req2) as res2:
+  #   print(res2)
 
-  sleep(1)
+  # sleep(1)
 
-  with urllib.request.urlopen(req3) as res3:
-    status = res3.read()
+  # with urllib.request.urlopen(req3) as res3:
+  #   status = res3.read()
+  #   print(status)
+
+  # sleep(1)
+
+  req_status = urllib.request.Request(status_url)
+  print(req_status.full_url)
+  with urllib.request.urlopen(req_status) as res_status:
+    status = res_status.read()
     print(status)
 
-  sleep(1)
+
+  req = urllib.request.Request('{}?{}'.format(capture_url, urllib.parse.urlencode(params)))
 
 
-
-  with urllib.request.urlopen(req4) as res4:
-    body = res4.read()
+  with urllib.request.urlopen(req) as res:
+    body = res.read()
     t = datetime.now().isoformat()
     filename = "./images/%s.jpg" % t
 
