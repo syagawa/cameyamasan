3fc33cbdd3113cf3206d401c5634b6bfed014ba9
syagawa <syagawapp@gmail.com>
2021-10-22 21:27:49 +0900

    Remove semicolon in SH1106.py

--- py/display/SH1106.py
+++ py/display/SH1106.py
@@ -37,32 +37,32 @@ class SH1106(object):
             return -1
         """Initialize dispaly"""    
         self.reset()
-        self.command(0xAE);#--turn off oled panel
-        self.command(0x02);#---set low column address
-        self.command(0x10);#---set high column address
-        self.command(0x40);#--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
-        self.command(0x81);#--set contrast control register
-        self.command(0xA0);#--Set SEG/Column Mapping     
-        self.command(0xC0);#Set COM/Row Scan Direction   
-        self.command(0xA6);#--set normal display
-        self.command(0xA8);#--set multiplex ratio(1 to 64)
-        self.command(0x3F);#--1/64 duty
-        self.command(0xD3);#-set display offset    Shift Mapping RAM Counter (0x00~0x3F)
-        self.command(0x00);#-not offset
-        self.command(0xd5);#--set display clock divide ratio/oscillator frequency
-        self.command(0x80);#--set divide ratio, Set Clock as 100 Frames/Sec
-        self.command(0xD9);#--set pre-charge period
-        self.command(0xF1);#Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
-        self.command(0xDA);#--set com pins hardware configuration
-        self.command(0x12);
-        self.command(0xDB);#--set vcomh
-        self.command(0x40);#Set VCOM Deselect Level
-        self.command(0x20);#-Set Page Addressing Mode (0x00/0x01/0x02)
-        self.command(0x02);#
-        self.command(0xA4);# Disable Entire Display On (0xa4/0xa5)
-        self.command(0xA6);# Disable Inverse Display On (0xa6/a7) 
+        self.command(0xAE)#--turn off oled panel
+        self.command(0x02)#---set low column address
+        self.command(0x10)#---set high column address
+        self.command(0x40)#--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
+        self.command(0x81)#--set contrast control register
+        self.command(0xA0)#--Set SEG/Column Mapping     
+        self.command(0xC0)#Set COM/Row Scan Direction   
+        self.command(0xA6)#--set normal display
+        self.command(0xA8)#--set multiplex ratio(1 to 64)
+        self.command(0x3F)#--1/64 duty
+        self.command(0xD3)#-set display offset    Shift Mapping RAM Counter (0x00~0x3F)
+        self.command(0x00)#-not offset
+        self.command(0xd5)#--set display clock divide ratio/oscillator frequency
+        self.command(0x80)#--set divide ratio, Set Clock as 100 Frames/Sec
+        self.command(0xD9)#--set pre-charge period
+        self.command(0xF1)#Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
+        self.command(0xDA)#--set com pins hardware configuration
+        self.command(0x12)
+        self.command(0xDB)#--set vcomh
+        self.command(0x40)#Set VCOM Deselect Level
+        self.command(0x20)#-Set Page Addressing Mode (0x00/0x01/0x02)
+        self.command(0x02)#
+        self.command(0xA4)# Disable Entire Display On (0xa4/0xa5)
+        self.command(0xA6)# Disable Inverse Display On (0xa6/a7) 
         time.sleep(0.1)
-        self.command(0xAF);#--turn on oled panel
+        self.command(0xAF)#--turn on oled panel
         
    
     def reset(self):
@@ -103,25 +103,25 @@ class SH1106(object):
     
     # def ShowImage(self,Image):
         # self.SetWindows()
-        # GPIO.output(self._dc, GPIO.HIGH);
+        # GPIO.output(self._dc, GPIO.HIGH)
         # for i in range(0,self.width * self.height/8):
             # config.spi_writebyte([~Image[i]])
             
     def ShowImage(self, pBuf):
         for page in range(0,8):
             # set page address #
-            self.command(0xB0 + page);
+            self.command(0xB0 + page)
             # set low column address #
-            self.command(0x02); 
+            self.command(0x02)
             # set high column address #
-            self.command(0x10); 
+            self.command(0x10)
             # write data #
             # time.sleep(0.01)
             if(self.Device == Device_SPI):
-                GPIO.output(self._dc, GPIO.HIGH);
+                GPIO.output(self._dc, GPIO.HIGH)
             for i in range(0,self.width):#for(int i=0;i<self.width; i++)
                 if(self.Device == Device_SPI):
-                    config.spi_writebyte([~pBuf[i+self.width*page]]); 
+                    config.spi_writebyte([~pBuf[i+self.width*page]])
                 else :
                     config.i2c_writebyte(0x40, ~pBuf[i+self.width*page])
                     
