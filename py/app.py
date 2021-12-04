d8a5a473b68123ab660998f69bf7a25cf31baefa
syagawa <syagawapp@gmail.com>
2021-12-04 14:47:43 +0900

    Add main to app.py

--- py/app.py
+++ py/app.py
@@ -2,3 +2,11 @@ from display.key import start_standby
 
 import controller
 from display.screen import make_screen
+
+
+def main():
+  s = make_screen
+  s["add"]("Start!")
+
+if __name__ == "__main__":
+  main()
