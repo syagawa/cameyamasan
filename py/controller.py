d538eef9568acf10a422d026d6dd43f8b0efe37b
syagawa <syagawapp@gmail.com>
2022-04-23 22:25:22 +0900

    Fix controller.py

--- py/controller.py
+++ py/controller.py
@@ -346,8 +346,8 @@ async def connect2(action_callback=None):
 
     do_action_callback("1 connect2 in cpy")
 
-    new_loop = asyncio.new_event_loop()
-    loop = asyncio.set_event_loop(new_loop)
+    # new_loop = asyncio.new_event_loop()
+    # loop = asyncio.set_event_loop(new_loop)
     # loop = asyncio.get_event_loop()
 
     # new_loop = asyncio.new_event_loop()
