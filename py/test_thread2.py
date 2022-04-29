a535aa38a9bf282d6cc5e90e4f6cdbf0d836f34f
syagawa <syagawapp@gmail.com>
2022-04-29 22:38:54 +0900

    Add test_thread2.py

---
+++ py/test_thread2.py
@@ -0,0 +1,20 @@
+import time
+import threading
+import asyncio
+
+
+def f1():
+  for n in range(10):
+    print("func1 {0}".format(n))
+    asyncio.sleep(1)
+
+def f2():
+  for n in range(20):
+    print("func2 {0}".format(n))
+    asyncio.sleep(0.5)
+
+if __name__ == "__main__":
+  thread1 = threading.Thread(target=f1)
+  thread2 = threading.Thread(target=f2)
+  thread1.start()
+  thread2.start()
