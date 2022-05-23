f891bc97294828e05c0788b0dbeecf84d571d005
syagawa <syagawapp@gmail.com>
2022-05-23 23:31:44 +0900

    Fix make_video.sh

--- make_video.sh
+++ make_video.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ls *.jpg | awk '{ printf "mv %s source%04d.jpg\n", $0, NR }' | sh
-ffmpeg -f image2 -r 15 -i source%04d.jpg -r 15 -an -vcodec libx264 -pix_fmt yuv420p video.mp4
+ls ./images/*.jpg | awk '{ printf "mv %s source%04d.jpg\n", $0, NR }' | sh
+# ffmpeg -f image2 -r 15 -i source%04d.jpg -r 15 -an -vcodec libx264 -pix_fmt yuv420p video.mp4
