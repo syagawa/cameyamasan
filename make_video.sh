60dd01b06119bf61c1aabcf3db3f2f7dff70dc3d
syagawa <syagawapp@gmail.com>
2022-05-26 23:41:48 +0900

    Update make_video.sh

--- make_video.sh
+++ make_video.sh
@@ -6,4 +6,5 @@ rm temp/*
 cp /images/${ARR[-1]}/* ./temp/
 
 ls ./temp/*.jpg | awk '{ printf "mv %s ./temp/source%04d.jpg\n", $0, NR }' | sh
-ffmpeg -f image2 -r 15 -i ./temp/source%04d.jpg -r 15 -an -vcodec libx264 -pix_fmt yuv420p ./temp/video.mp4
+ffmpeg -f image2 -r 3 -i ./temp/source%04d.jpg -r 3 -an -vcodec libx264 -pix_fmt yuv420p ./temp/video.mp4
+
