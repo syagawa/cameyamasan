ce93046046d1c040f579440b67a9d8d795331f62
syagawa <syagawapp@gmail.com>
2022-05-26 23:41:38 +0900

    Add make_tar.sh

---
+++ make_tar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+eval ARR=("$(ls /images/ --quoting-style=shell)")
+mkdir -p ./temp
+rm temp/*
+cp /images/${ARR[-1]}/* ./temp/
+
+ls ./temp/*.jpg | awk '{ printf "mv %s ./temp/source%04d.jpg\n", $0, NR }' | sh
+
+find ./temp/ -name *.jpg | xargs tar -cvzf ./temp/images.tar.gz
